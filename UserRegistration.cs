d2ca83279d7986d0537e26bc860a935215c3205e
VIGNESH05121996 <90402981+VIGNESH05121996@users.noreply.github.com>
2021-11-04 10:56:30 +0530

    Update UserRegistration.cs

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -70,7 +70,7 @@ namespace RegularExpressions
             }
             else
             {
-                Console.WriteLine("Invalid email-Please Check Your email");
+                Console.WriteLine("Invalid Mobile Number-Please Check Your email");
             }
         }
 
@@ -86,7 +86,7 @@ namespace RegularExpressions
             }
             else
             {
-                Console.WriteLine("Invalid email-Please Check Your password");
+                Console.WriteLine("Invalid password-Please Check Your password");
             }
         }
     }

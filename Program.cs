798609d61d23a3b6b1bce57f4f227548cd8f40ee
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-11-18 20:30:31 +0530

    [VIGNESH M] Added: AllValidEmails Method,Refractored previous code and added comments

--- Program.cs
+++ Program.cs
@@ -8,14 +8,60 @@ namespace RegularExpressions
         {
             Console.WriteLine("Welcome To Regular Expressions User Registration");
             UserRegistration user = new UserRegistration();
-            user.FirstName();
-            user.LastName();
-            user.ValidEmail();
-            user.MobileNumber();
-            user.ValidPasswordRule1();
-            user.ValidPasswordRule2();
-            user.ValidPasswordRule3();
-            user.ValidPasswordRule4();
+            for(int i=1;i<=9;i++)
+            {
+                switch(i)
+                {
+                    case 1:
+                        Console.WriteLine("\nValidating First Name");
+                        user.FirstName();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nValidating Last Name");
+                        user.LastName();
+                        break;
+
+                    case 3:
+                        Console.WriteLine("\nValidating Email");
+                        user.ValidEmail();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("\nValidating Mobile Number");
+                        user.MobileNumber();
+                        break;
+
+                    case 5:
+                        Console.WriteLine("\nValidating Password Rule 1");
+                        user.ValidPasswordRule1();
+                        break;
+
+                    case 6:
+                        Console.WriteLine("\nValidating Password Rule 2");
+                        user.ValidPasswordRule2();
+                        break;
+
+                    case 7:
+                        Console.WriteLine("\nValidating Password Rule 3");
+                        user.ValidPasswordRule3();
+                        break;
+
+                    case 8:
+                        Console.WriteLine("\nValidating Password Rule 4");
+                        user.ValidPasswordRule4();
+                        break;
+
+                    case 9:
+                        Console.WriteLine("\nValidating All Email Rules");
+                        user.AllValidEmails();
+                        break;
+
+                    default:
+                        Console.WriteLine("\nAll Used Cases Are Over");
+                        break;
+                }
+            }
         }
     }
 }
--- UserRegistration.cs
+++ UserRegistration.cs
@@ -12,129 +12,164 @@ namespace RegularExpressions
     {
         public void FirstName()
         {
-            string firstName = "^[A-Z]{1}[a-zA-Z]{2,}";
-            Console.WriteLine("Enter First Name:");
-            string name = Convert.ToString(Console.ReadLine());
-            Regex regex = new Regex(firstName);
-            if (regex.IsMatch(name))
+            string firstName = "^[A-Z]{1}[a-zA-Z]{2,}"; //Regular Expression shows that firstName starts with caps and minimum of 3 character
+            string[] name = { "Vig", "vig", "Vignesh", "VIg", "Vig" }; //set of names to be validated
+            Regex regex = new Regex(firstName); //calling Regex function
+            foreach(string word in name)
             {
-                Console.WriteLine(name + " is valid");
-            }
-            else
-            {
-                Console.WriteLine("Invalid name(First Name should be in capital)-Please Check Your Name");
+                if (regex.IsMatch(word))
+                {
+                    Console.WriteLine(word + " is valid");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid name(First Name should be in capital)-Please Check Your Name");
+                }
             }
         }
 
         public void LastName()
         {
-            string lastName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+            string lastName = "^[A-Z]{1}[a-zA-Z]{2,}$"; //Regular Expression shows that lastName starts with caps and minimum of 3 character
             Console.WriteLine("Enter Last Name:");
-            string name = Convert.ToString(Console.ReadLine());
-            Regex regex = new Regex(lastName);
-            if (regex.IsMatch(name))
+            string[] name = { "Mut", "mut", "Muthu", "MUt", "Mut" }; //set of last name to be validated
+            Regex regex = new Regex(lastName); //calling Regex function
+            foreach(string word in name)
             {
-                Console.WriteLine(name + " is valid");
-            }
-            else
-            {
-                Console.WriteLine("Invalid name(Last Name should be in capital)-Please Check Your Name");
+                if (regex.IsMatch(word))
+                {
+                    Console.WriteLine(word + " is valid");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid name(Last Name should be in capital)-Please Check Your Name");
+                }
             }
         }
 
         public void ValidEmail()
         {
-            string email = @"^[a-zA-Z0-9]+[+-._]?[a-zA-Z0-9]*[+-._]?[a-zA-Z0-9]+@[a-zA-Z0-9]+[.]{1}[a-zA-Z]{2,3}[.]?[a-zA-Z]{0,3}$";
-            Console.WriteLine("Enter Email:");
-            string mail = Convert.ToString(Console.ReadLine());
-            Regex regex = new Regex(email);
-            if (regex.IsMatch(mail))
+            string email = "^[a-z]{3}[.][a-z]*[@]{1}[bl]{2}[.]{1}[co]{2}[.]{1}[a-z]*$"; //Regular Expression shows Email should have 3 mandatory parts(abc,bl &co) and 2 optional(xyz & in) with precise @ and . position
+            string[] mail = { "abc.xyz@bl.co.in", "abc@bl.co.in", "abc.x@gm.co.in", "abc.xyzbl.co.in", "abc,xyz@bl.in", "ab.xyz@bl.co.in", "abc.nxcst@bl.co.in" }; //set of emayl to be validated
+            Regex regex = new Regex(email); //calling Regex function
+            foreach(string set in mail)
             {
-                Console.WriteLine(mail + " is valid");
-            }
-            else
-            {
-                Console.WriteLine("Invalid email-Please Check Your email");
+                if (regex.IsMatch(set))
+                {
+                    Console.WriteLine(set + " is valid");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid email-Please Check Your email");
+                }
             }
         }
 
         public void MobileNumber()
         {
-            string mobile = "^([0-9]{2}[ ][0-9]{10})|([0-9]{3}[ ][0-9]{10})$";
-            Console.WriteLine("Enter Mobile Number:");
-            string number = Convert.ToString(Console.ReadLine());
-            Regex regex = new Regex(mobile);
-            if (regex.IsMatch(number))
+            string mobile = "^([0-9]{2}[ ][0-9]{10})|([0-9]{3}[ ][0-9]{10})$"; //Regular Expression shows that number should contain country code followed by space and 10 digit number
+            string[] number = { "91 7656511056", "01 7486631255", "715682331652", "91 75866328", "91 0123456789", "91 768697195312" }; //set of mobile numbers to be validated
+            Regex regex = new Regex(mobile); //calling Regex function
+            foreach(string phno in number )
             {
-                Console.WriteLine(number + " is valid");
-            }
-            else
-            {
-                Console.WriteLine("Invalid Mobile Number-Please Check Your email");
+                if (regex.IsMatch(phno))
+                {
+                    Console.WriteLine(phno + " is valid");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Mobile Number-Please Check Your email");
+                }
             }
         }
 
         public void ValidPasswordRule1()
         {
-            string password = "^[a-zA-Z0-9]{8,}$";
-            Console.WriteLine("Enter Passwrod:");
-            string pass = Convert.ToString(Console.ReadLine());
-            Regex regex = new Regex(password);
-            if (regex.IsMatch(pass))
+            string password = "^[a-zA-Z0-9]{8,}$"; //Regular expression shows that password should contain minimum 8 characters
+            string[] pass = { "mvignesh", "mvig", "Mvignesh", "mvignesh0512", "0512mvig" }; //set of passwords to be validated
+            Regex regex = new Regex(password); //calling Regex function
+            foreach(string set in pass)
             {
-                Console.WriteLine(pass + " is valid");
-            }
-            else
-            {
-                Console.WriteLine("Invalid password-Please Check Your password");
+                if (regex.IsMatch(set))
+                {
+                    Console.WriteLine(set + " is valid");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid password-Please Check Your password");
+                }
             }
         }
 
         public void ValidPasswordRule2()
         {
-            string password = "^(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9]{8}$";
-            Console.WriteLine("Enter Passwrod:");
-            string pass = Convert.ToString(Console.ReadLine());
-            Regex regex = new Regex(password);
-            if (regex.IsMatch(pass))
+            string password = "^[a-zA-Z0-9]{8,}$"; //Regular expression shows that password should have atleast 1 upper case
+            string[] pass = { "V1ignesh", "Vignesh1", "Vigne4sh", "Vignesh1", "Mvignesh", "mvigNesg1", "mvignesh05" }; //set of passwords to be validated
+            Regex regex = new Regex(password); //calling Regex function
+            foreach (string set in pass)
             {
-                Console.WriteLine(pass + " is valid");
-            }
-            else
-            {
-                Console.WriteLine("Invalid password-Please Check Your password");
+                if (regex.IsMatch(set))
+                {
+                    Console.WriteLine(set + " is valid");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid password-Please Check Your password");
+                }
             }
         }
 
         public void ValidPasswordRule3()
         {
-            string password = "^(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9]{8}$";
-            Console.WriteLine("Enter Passwrod:");
-            string pass = Convert.ToString(Console.ReadLine());
-            Regex regex = new Regex(password);
-            if (regex.IsMatch(pass))
+            string password = "^(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9]{8}$"; //Regular expression shows that should have atleast 1 numeric number in the password
+            string[] pass = { "V1ignesh", "Vignesh1", "Vigne4sh", "Vignesh1", "Mvignesh", "mvigNeshg1", "mvignesh05" };  //set of passwords to be validated
+            Regex regex = new Regex(password); //calling Regex function
+            foreach (string set in pass)
             {
-                Console.WriteLine(pass + " is valid");
-            }
-            else
-            {
-                Console.WriteLine("Invalid password-Please Check Your password");
+                if (regex.IsMatch(set))
+                {
+                    Console.WriteLine(set + " is valid");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid password-Please Check Your password");
+                }
             }
         }
 
         public void ValidPasswordRule4()
         {
-            string password = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8}$";
-            Console.WriteLine("Enter Passwrod:");
-            string pass = Convert.ToString(Console.ReadLine());
-            Regex regex = new Regex(password);
-            if (regex.IsMatch(pass))
+            string password = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$"; //Regular expression shows that has exactly one special case
+            string[] pass = { "V1igne@sh", "Vignesh&2", "Vigne%5sh", "Mvig!nesh", "Mvig@nesh1", "mvignesh!" }; //set of passwords to be validated
+            Regex regex = new Regex(password); //calling Regex function
+            foreach (string set in pass)
             {
-                Console.WriteLine(pass + " is valid");
+                if (regex.IsMatch(set))
+                {
+                    Console.WriteLine(set + " is valid");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid password-Please Check Your password");
+                }
             }
-            else
+        }
+
+        public void AllValidEmails()
+        {
+            string email = @"^[a-zA-Z0-9]+[+-._]?[a-zA-Z0-9]*[+-._]?[a-zA-Z0-9]+@[a-zA-Z0-9]+[.]{1}[a-zA-Z]{2,3}[.]?[a-zA-Z]{0,3}$"; //Regular Expression shows to test all valid email
+            string[] mail = { "1.abc@yahoo.com", "2.abc-100@yahoo.com", "3.abc.100@yahoo.com", "2.abc111@abc.com", "4.abc-100@abc.net", "5.abc.100@abc.com.au", "6.abc@1.com", "7.abc@gmail.com.com", "8.abc+100@gmail.com" }; //set of emayl to be validated
+            Regex regex = new Regex(email); //calling Regex function
+            foreach (string set in mail)
             {
-                Console.WriteLine("Invalid password-Please Check Your password");
+                if (regex.IsMatch(set))
+                {
+                    Console.WriteLine(set + " is valid");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid email-Please Check Your email");
+                }
             }
         }
     }
